fe1c7f55cd84dde99d370c48d12d642464a7a459
Kubernetes Publisher <k8s-publishing-bot@users.noreply.github.com>
2019-03-08 09:03:33 +0000

    Merge remote-tracking branch 'origin/master' into release-1.14
    Kubernetes-commit: 2676471cfcb269267a16a2c7cc2df369944ffcc2

--- vendor/k8s.io/api/node/v1alpha1/doc.go
+++ vendor/k8s.io/api/node/v1alpha1/doc.go
@@ -1,5 +1,5 @@
 /*
-Copyright 2016 The Kubernetes Authors.
+Copyright 2018 The Kubernetes Authors.
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -15,7 +15,8 @@ limitations under the License.
 */
 
 // +k8s:deepcopy-gen=package
-// +k8s:protobuf-gen=package
 // +k8s:openapi-gen=true
 
-package v1
+// +groupName=node.k8s.io
+
+package v1alpha1
