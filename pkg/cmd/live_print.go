e0ed1635037769501a1769cf4c5bb7adf01bd030
Eric Yang <shyang@firstam.com>
2023-09-05 13:27:56 -0700

    Make sure the colors index never ran out of range if there are so many lines

--- pkg/cmd/live_print.go
+++ pkg/cmd/live_print.go
@@ -24,19 +24,22 @@ func LivePrint(lines []string) {
 func ColorPrintLines(lines []string) {
 	colors := [...]*Color{
 		color.New(color.FgCyan),
+		color.New(color.FgYellow),
+		color.New(color.FgMagenta),
 		color.New(color.FgWhite),
-		color.New(color.FgRed),
 		color.New(color.FgGreen),
 		color.New(color.FgBlue)}
 
 	for index := 0; index < len(lines); index++ {
-		colors[index].Println(lines[index] + " " + strconv.Itoa(index))
+		colorIndex := index % len(colors)
+		colors[colorIndex].Println(lines[index] + " " + strconv.Itoa(index))
 	}
 }
 
-// Mocking the functions that the plugin will generate for print
+// MockLines the functions that the plugin will generate for print
 func MockLines(count int) []string {
-	testText := "pod1 - running - live count:\npod2 - running - live count:\npod3 - running - live count:\npod4 - running - live count:"
+	testText := "pod1 - running - live count:\npod2 - running - live count:\npod3 - running - live count:\npod4 - running - live count:\n"
+	testText = testText + strings.Repeat(testText, 3)
 	testOutput := strings.Split(testText, "\n")
 
 	for index := 0; index < len(testOutput); index++ {
